1ae7f6237d17e7538e7d2c51c4f8768c1cf370f9
iamnotsnani <102783684+MCCode06@users.noreply.github.com>
2025-02-01 01:15:34 +0400

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
         Player player = new Player(50, 50, 30, 30, 5);
         player.moveDown();
